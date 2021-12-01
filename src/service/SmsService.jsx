9cc7c8e55a7aa69127c41114daf21bb374af10c3
pedram Ezzati <pedinil@gmail.com>
2021-12-01 22:04:05 +0100

    Update SmsService.jsx

--- src/service/SmsService.jsx
+++ src/service/SmsService.jsx
@@ -1,17 +1,18 @@
-import { Sendernumber } from "variables/config";
 export const SMSCall = (to,text) => {
 
-
-  var randomNumber = Math.floor(Math.random() * Sendernumber.length);
-  
   var myHeaders = new Headers();
-  myHeaders.append("Authorization", "Basic QUM0ODJiOTFkZWI3MjdiZmFjZTk1NWVhYzZmODRmYjczNDpjNGYxZDcxOWM5NDY0YzI4YjY1YzZlMjY0NGUzMzg0Yg==");
+  //myHeaders.append("Authorization", "Basic QUM0ODJiOTFkZWI3MjdiZmFjZTk1NWVhYzZmODRmYjczNDpjNGYxZDcxOWM5NDY0YzI4YjY1YzZlMjY0NGUzMzg0Yg==");
+  
+  myHeaders.append("Authorization", "Basic QUNiNGRjOWJkYmViZTkzYjFhZmM2NWZkZGMzNTZhOGZiNTphMDMyNjEwNWFjMmU0ZjhkMzcwYmI4NTEyY2E1ZTE1MQ==");
   myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
 
 
   var urlencoded = new URLSearchParams();
 urlencoded.append("Body", text);
-urlencoded.append("From", "+"+Sendernumber[randomNumber]);
+
+//urlencoded.append("From", "+12054486735");
+
+urlencoded.append("From", "+17164552902");
 urlencoded.append("To", "+1"+to);
  
   
@@ -22,7 +23,8 @@ var requestOptions = {
   redirect: 'follow'
 };
   
-  return fetch("https://api.twilio.com/2010-04-01/Accounts/AC482b91deb727bface955eac6f84fb734/Messages.json", requestOptions).then((response)=>{
+//return fetch("https://api.twilio.com/2010-04-01/Accounts/AC482b91deb727bface955eac6f84fb734/Messages.json"
+  return fetch("https://api.twilio.com/2010-04-01/Accounts/ACb4dc9bdbebe93b1afc65fddc356a8fb5/Messages.json", requestOptions).then((response)=>{
     return response
   })
 }
